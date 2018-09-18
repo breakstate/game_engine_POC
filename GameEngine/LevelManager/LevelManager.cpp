8d9d44758306c9d9af26983dfdda779887e2bd28
Brenton Moodley <bmoodley@student.42.fr>
2018-09-18 23:30:23 +0200

    map generating solid walls in bomberman style grid internally

--- GameEngine/LevelManager/LevelManager.cpp
+++ GameEngine/LevelManager/LevelManager.cpp
@@ -21,19 +21,32 @@ void	LevelManager::generateMap( void ){
 	// abundance of enemies and breakable bricks depends on level
 	// (total vector elements / y) + x
 	// totalBlocks = x*y
+	int block = 1;
+	int type;
+	int line = 1;
 	for (int i = 0; i <= 81; i++){
+		type = 0;
 		if (((i%9 == 1) || (i%9 == 0)) || ((i <= 9) || (i >= 81 - 9))){
-			std::cout << i << " and 1" << std::endl; // debug
-			this->testMap.push_back(1);
+			type = 1;
+			block = 0;
 		}
 		else{
-			std::cout << i << " and 0" << std::endl; // debug
-			this->testMap.push_back(0);
+			if ((i >= (this->mapWidth * 2 - 1)) && (i < (this->mapWidth * this->mapHeight - this->mapWidth * 2 - 1))){
+				line++;
+				block++;
+				if (block % 2 == 0 && line % 2 == 0)
+					type = 1;
+				else
+					type = 0;
+			}
 		}
+		this->testMap.push_back(type);
 	}
+	this->testMap.pop_back();
 }
 
 void	LevelManager::debugPrintMap( void ){ // debug // test
+	std::cout << testMap.size() << std::endl;
 	for (int i = 1; i <= testMap.size(); i++){
 		std::cout << testMap[i];
 		if (i % 9 == 0)
